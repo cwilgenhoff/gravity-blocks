bae3f3d2d13a90b7f7c414f8859a563cd7f02fb5
Cristian Wilgenhoff <cpwilgenhoff@gmail.com>
2017-04-22 21:34:28 +0100

    Implemented algorithm to find all blocks given a selected one

--- app/javascript/grid.js
+++ app/javascript/grid.js
@@ -11,30 +11,39 @@ export class Block {
 }
 
 export class BlockGrid {
-  constructor() {
-    this.grid = [];
-
-    for (let x = 0; x < MAX_X; x++) {
-      const col = [];
-      for (let y = 0; y < MAX_Y; y++) {
-        col.push(new Block(x, y));
-      }
-
-      this.grid.push(col);
-    }
+  constructor({ width = MAX_X, height = MAX_Y } = {}) {
+    this.size = {
+      width,
+      height,
+    };
+    this.grid = this.generateGrid();
 
     return this;
   }
 
+  generateGrid() {
+    const grid = [];
+    for (let x = 0; x < this.size.width; x++) {
+      const col = [];
+      for (let y = 0; y < this.size.height; y++) {
+        col.push(new Block(x, y));
+      }
+
+      grid.push(col);
+    }
+
+    return grid;
+  }
+
   render(el = document.querySelector('#gridEl')) {
-    for (let x = 0; x < MAX_X; x++) {
+    for (let x = 0; x < this.size.width; x++) {
       const colId = `col_${x}`;
       const colEl = document.createElement('div');
       colEl.className = 'col';
       colEl.id = colId;
       el.appendChild(colEl);
 
-      for (let y = MAX_Y - 1; y >= 0; y--) {
+      for (let y = this.size.height - 1; y >= 0; y--) {
         const block = this.grid[x][y];
         const blockId = `block_${x}x${y}`;
         const blockEl = document.createElement('div');
@@ -50,8 +59,54 @@ export class BlockGrid {
     return this;
   }
 
+  reset() {
+    const el = document.querySelector('#gridEl');
+    el.innerHTML = '';
+  }
+
   blockClicked(e, block) {
-    console.log(e, block);
+    e.preventDefault();
+
+    const blocks = [];
+    this.findBlocks(block, blocks);
+    console.log(blocks);
+  }
+
+  findBlocks(block, blocks) {
+    if (!block) {
+      return false;
+    }
+
+    if (blocks.includes(block)) {
+      return false;
+    }
+
+    blocks.push(block);
+
+    const searchPath = [
+      this.getFromGrid(block.x - 1, block.y),
+      this.getFromGrid(block.x, block.y + 1),
+      this.getFromGrid(block.x + 1, block.y),
+      this.getFromGrid(block.x, block.y - 1),
+    ];
+
+    searchPath.forEach(target => {
+      if (target && this.isEqualBlock(block, target)) {
+        this.findBlocks(target, blocks);
+      }
+    });
+
+    return blocks;
+  }
+
+  getFromGrid(x, y) {
+    if (this.grid[x] && this.grid[x][y]) {
+      return this.grid[x][y];
+    }
+  }
+
+  isEqualBlock(leftBlock, rightBlock) {
+    return leftBlock && rightBlock && leftBlock.colour === rightBlock.colour;
   }
 }
 
--- app/javascript/grid.test.js
+++ app/javascript/grid.test.js
@@ -1,12 +1,12 @@
-import { Block, COLOURS } from './grid';
 import { assert } from 'chai';
+import { Block, BlockGrid, COLOURS } from './grid';
 
 describe('Block', () => {
   it('should be created with correct coordinates and one of the valid colours', () => {
-    let testCoords = [[1, 2], [4, 9], [0, 0]];
+    const testCoords = [[1, 2], [4, 9], [0, 0]];
 
     testCoords.forEach(testCoord => {
-      let block = new Block(...testCoord);
+      const block = new Block(...testCoord);
       assert.equal(block.x, testCoord[0], 'x is set correctly');
       assert.equal(block.y, testCoord[1], 'y is set correctly');
       assert.ok(COLOURS.indexOf(block.colour) > -1, 'colour is valid');
@@ -15,5 +15,304 @@ describe('Block', () => {
 });
 
 describe('BlockGrid', () => {
-  xit('should be implemented!', () => {});
+  let blockGrid;
+
+  it('should exist', () => {
+    blockGrid = new BlockGrid();
+
+    assert.isNotNull(blockGrid, 'grid exists');
+  });
+
+  it('should have a default size of 10x10', () => {
+    blockGrid = new BlockGrid();
+
+    assert.equal(blockGrid.grid.length, 10);
+    blockGrid.grid.forEach(column => assert.equal(column.length, 10));
+  });
+
+  it('should have provided size when created', () => {
+    blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+    assert.equal(blockGrid.grid.length, 3);
+    blockGrid.grid.forEach(column => assert.equal(column.length, 3));
+  });
+
+  describe('blockClicked', () => {
+
+  });
+
+  describe('findBlocks', () => {
+    let target;
+    let neighbour;
+
+    it('should find all blocks including the one on the left', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(0, 1);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, neighbour, null],
+        [null, target, null],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 2);
+      blocks.forEach(block => {
+        assert.equal(block.colour, 'blue');
+      });
+    });
+
+    it('should find only the given block (no top left)', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(0, 2);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, neighbour],
+        [null, target, null],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 1);
+    });
+
+    it('should find all blocks including the one on the top', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 2);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [null, target, neighbour],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 2);
+      blocks.forEach(block => {
+        assert.equal(block.colour, 'blue');
+      });
+    });
+
+    it('should find only the given block (no top right)', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(2, 2);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [null, target, null],
+        [null, null, neighbour],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 1);
+    });
+
+    it('should find all blocks including the one on the right', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(2, 1);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [null, target, null],
+        [null, neighbour, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 2);
+    });
+
+    it('should find only the given block (no bottom right)', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(2, 0);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [null, target, null],
+        [neighbour, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 1);
+    });
+
+    it('should find all blocks including the one on the bottom', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 0);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [neighbour, target, null],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 2);
+    });
+
+    it('should find only the given block (no bottom left)', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(2, 0);
+      neighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [neighbour, null, null],
+        [null, target, null],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 1);
+    });
+
+    it('should find all blocks including the two on the left', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(2, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 1);
+      neighbour.colour = 'blue';
+
+      const secondNeighbour = new Block(0, 1);
+      secondNeighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, secondNeighbour, null],
+        [null, neighbour, null],
+        [null, target, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 3);
+    });
+
+    it('should find all blocks including the two on the right', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(0, 1);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 1);
+      neighbour.colour = 'blue';
+
+      const secondNeighbour = new Block(2, 1);
+      secondNeighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, target, null],
+        [null, neighbour, null],
+        [null, secondNeighbour, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 3);
+    });
+
+    it('should find all blocks including the two on the top', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 0);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 1);
+      neighbour.colour = 'blue';
+
+      const secondNeighbour = new Block(1, 2);
+      secondNeighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [target, neighbour, secondNeighbour],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 3);
+    });
+
+    it('should find all blocks including the two on the bottom', () => {
+      blockGrid = new BlockGrid({ width: 3, height: 3 });
+
+      target = new Block(1, 2);
+      target.colour = 'blue';
+
+      neighbour = new Block(1, 1);
+      neighbour.colour = 'blue';
+
+      const secondNeighbour = new Block(1, 0);
+      secondNeighbour.colour = 'blue';
+
+      blockGrid.grid = [
+        [null, null, null],
+        [secondNeighbour, neighbour, target],
+        [null, null, null],
+      ];
+
+      const blocks = [];
+      blockGrid.findBlocks(target, blocks);
+
+      assert.equal(blocks.length, 3);
+    });
+  });
 });
